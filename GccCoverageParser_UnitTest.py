a9c5bbb474ef34be32bd057960f837fd455fe7de
Fasten90 <fasten90@gmail.com>
2019-11-01 20:08:51 +0100

    # UnitTest - 2. gcov data parseing test

--- GccCoverageParser_UnitTest.py
+++ GccCoverageParser_UnitTest.py
@@ -1,6 +1,7 @@
-from Run_GccCoverage_ForSourceFiles import gcov_info, get_line_data
 
 import unittest
+
+from Run_GccCoverage_ForSourceFiles import gcov_info, get_line_data, regex_function_detect
 
 
 class GccCoverageParser(unittest.TestCase):
@@ -31,6 +32,78 @@ class GccCoverageParser(unittest.TestCase):
             res = get_line_data(test_data)
             check_res = res[0]
             assert (check_res == expected_result)
+
+    def test_function_detection_is_function(self):
+        function_ok_list = \
+"""
+void function1(void)
+
+void function2(void) {
+
+void function3(int blabla)
+
+void function4(int blabla) {
+
+int function5(void)
+
+int * function6(int bla1, int bla2)
+
+INLINE_FUNCTION void function7(int blabla)
+
+void function8 ( int * bla )
+
+void function9 ( uint8_t * ehh, Type_ omg )
+
+bool BUTTON_GetButtonState(ButtonType_t button)
+"""
+        # not null lines is a test
+        for line in function_ok_list.split("\n"):
+            if line.strip():
+                # Not null line
+                # Be careful! Check the full line!
+                res = regex_function_detect.match(line)
+                assert (res)
+
+
+    def test_function_detection_is_not_function(self):
+        function_not_list = \
+"""
+Wrong unittest
+/* Noooooooooooooo */
+/* Do not accept because the empty () */
+void function()
+
+    IO_Output_SetStatus(IO_LED_Blue, IO_Output_Cmd_SetToggle);
+
+    UNUSED_ARGUMENT(source);
+
+
+    if (UnitTest_InvalidCnt)
+
+    state = (HAL_GPIO_ReadPin(BUTTON_USER_GPIO_PORT, BUTTON_USER_GPIO_PIN) == GPIO_PIN_SET) ? true : false;
+
+
+    else if (str[1] == 80)
+
+    else if (circBuff->readCnt > circBuff->writeCnt)
+
+    else if (!StrCmp("settime", argv[1]) && argc == 3)
+
+    else if (Logic_Display_ActualState < AppType_Count)
+
+    else if (TaskList[i].isRequestScheduling)
+
+/* Commented line */
+     *         Test data in end of buffer (overflow!)
+
+"""
+        # not null lines is a test
+        for line in function_not_list.split("\n"):
+            if line.strip():
+                # Not null line
+                # Be careful! Check the full line!
+                res = regex_function_detect.match(line)
+                assert (not res)
 
 
 if __name__ == '__main__':
